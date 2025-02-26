92d9c73ffe9b815e1172bd12a8f523fe7742c4ef
ishanavnt-01 <133764641+ishanavnt-01@users.noreply.github.com>
2025-02-26 10:37:56 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -31,18 +31,20 @@ fi
 cd /var/www/driver-new-tech
 sudo cp driver-app.conf nginx/driver.conf
 echo "copied file from driver-app.conf"
-sudo sed -i -e "s/HOST_NAME/${HOST_NAME}/g" nginx/driver.conf
-echo $HOST_NAME
-sudo sed -i -e "s,STATIC_ROOT,$STATIC_ROOT,g" nginx/driver.conf
-echo $STATIC_ROOT
-sudo sed -i -e "s,DIST_ROOT,$DIST_ROOT,g" nginx/driver.conf
-echo $DIST_ROOT
-echo "replacing variables"
-sudo sed -e "s/driver-new-tech/${DJANGO_HOST}/g" nginx/driver.conf
-echo $DJANGO_HOST
-sudo sed -e "s/driver-celery/${CELERY_HOST}/g" nginx/driver.conf
-echo $CELERY_HOST
-sudo sed -e "s/windshaft/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' windshaft)/g" nginx/driver.conf
+sudo sed -i -e "s/HOST_NAME/${HOST_NAME}/g" nginx/driver.conf && sudo sed -i -e "s,STATIC_ROOT,$STATIC_ROOT,g" nginx/driver.conf && sudo sed -i -e "s,DIST_ROOT,$DIST_ROOT,g" nginx/driver.conf && sudo sed -e "s/driver-new-tech/${DJANGO_HOST}/g" nginx/driver.conf && sudo sed -e "s/driver-celery/${CELERY_HOST}/g" nginx/driver.conf && sudo sed -e "s/windshaft/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' windshaft)/g" nginx/driver.conf
+
+# sudo sed -i -e "s/HOST_NAME/${HOST_NAME}/g" nginx/driver.conf
+# echo $HOST_NAME
+# sudo sed -i -e "s,STATIC_ROOT,$STATIC_ROOT,g" nginx/driver.conf
+# echo $STATIC_ROOT
+# sudo sed -i -e "s,DIST_ROOT,$DIST_ROOT,g" nginx/driver.conf
+# echo $DIST_ROOT
+# echo "replacing variables"
+# sudo sed -e "s/driver-new-tech/${DJANGO_HOST}/g" nginx/driver.conf
+# echo $DJANGO_HOST
+# sudo sed -e "s/driver-celery/${CELERY_HOST}/g" nginx/driver.conf
+# echo $CELERY_HOST
+# sudo sed -e "s/windshaft/$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' windshaft)/g" nginx/driver.conf
 echo "replaced variables"
 
 ## sudo docker exec driver-nginx sed -i -e "s/HOST_NAME/${HOST_NAME}/g" /etc/nginx/conf.d/driver-app.conf
