63c822715cd9679fc6b56d9f2010cf75451d4c18
khijar-shaikh <khijar.shaikh@aventior.com>
2025-03-06 18:11:31 +0530

    Create gcp-pre-installation.sh

---
+++ gcp-pre-installation.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
+export $(cat setup.conf)
+echo $ECR_REPO
+if ! command -v docker &> /dev/null; then
+    echo "Docker not found, installing..."
+    sudo apt-get update -y
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y docker-ce docker-ce-cli containerd.io
+    sudo curl -L https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+
+# Ensure docker group exists and add the current user to it
+if ! getent group docker > /dev/null; then
+    echo "Docker group does not exist. Creating docker group..."
+    sudo groupadd docker
+fi
+
+# Add the current user to the docker group
+sudo usermod -aG docker $USER
+echo "User $USER added to docker group. You may need to log out and log back in for the changes to take effect."
+
+if ! command -v nginx &> /dev/null; then
+    echo "nginx not found, installing..."
+sudo apt-get update -y
+sudo apt-get install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
+fi
+
+if ! command -v psql &> /dev/null; then
+    echo "psql not found, installing..."
+sudo apt-get update -y
+sudo apt install postgresql-client -y
+fi
+
+if [ ! -d "/var/www/driver-new-tech/" ]; then
+    # Create the directory
+    sudo mkdir -p /var/www/driver-new-tech/
+    
+    # Change ownership to the desired user and group (replace 'user' and 'group' accordingly)
+    
+    # Set the correct permissions (e.g., 755, or adjust based on your needs)
+    sudo chmod -R 755 /var/www/driver-new-tech/
+    
+    echo "Directory created, ownership and permissions set."
+else
+    echo "Directory already exists. No changes made."
+fi
+
